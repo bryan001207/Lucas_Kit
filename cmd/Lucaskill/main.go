3e3b0722795089154309c2c1114ebd994de45c59
Lucas <lucasmangroelal@hotmail.nl>
2026-01-29 17:46:28 +0100

    feat: release v3.2.4

--- cmd/Lucaskill/main.go
+++ cmd/Lucaskill/main.go
@@ -19,7 +19,7 @@ import (
 	"github.com/miekg/dns"
 )
 
-const version = "2.1.3"
+const version = "3.2.4"
 
 func printBanner() {
 	fmt.Println("Lucaskill is made by Lucas Mangroelal | lucasmangroelal.nl")
--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 	"github.com/miekg/dns"
 )
 
-const version = "2.1.3"
+const version = "3.2.4"
 
 func printBanner() {
 	fmt.Println("Lucas DNS is made by Lucas Mangroelal | lucasmangroelal.nl")
